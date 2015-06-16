d5b88db4369a4caf213b20dadd009afd955ee852
bmatics <bmatics@viimed.com>
2015-06-16 12:12:56 -0400

    only replacing ilike if surrounded by whitespace

--- src/Database/Query/Grammars/SQLiteIlikeGrammar.php
+++ src/Database/Query/Grammars/SQLiteIlikeGrammar.php
@@ -9,23 +9,23 @@ class SQLiteIlikeGrammar extends SQLiteGrammar
 	protected function compileWheres(Builder $query)
 	{
 		$compiled = parent::compileWheres($query);
-		return $this->handleIlikes($compiled);
+		return $this->replaceIlikes($compiled);
 	}
 
 	protected function compileJoins(Builder $query, $joins)
 	{
 		$compiled = parent::compileJoins($query, $joins);
-		return $this->handleIlikes($compiled);
+		return $this->replaceIlikes($compiled);
 	}
 
 	protected function compileHavings(Builder $query, $havings)
 	{
 		$compiled = parent::compileHavings($query, $havings);
-		return $this->handleIlikes($compiled);		
+		return $this->replaceIlikes($compiled);		
 	}
 
-	protected function handleIlikes($compiled)
+	protected function replaceIlikes($compiled)
 	{
-		return preg_replace('/ilike/i', 'like', $compiled);
+		return preg_replace('/(\s)ilike(\s)/i', '$1like$2', $compiled);
 	}
 }
